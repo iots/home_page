c065771b209eb0cd83700142fb2dfcff2f449c0c
ohlsj <ohlsj@163.com>
2015-07-31 14:10:53 +0800

    first homepage test

--- home_page/home_show/tests.py
+++ home_page/home_show/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.test import Client
+from .models import Person
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        Person.objects.create(name='online', number=65535)
+        Person.objects.create(name='read', number=4096)
+        Person.objects.create(name='sent', number=1024)
+
+    def testHomeView(self):
+        response = self.client.get('/home_show/home/')
+        self.assertEqual(response.status_code, 200)
+
+    def testQueryView(self):
+        response = self.client.get('/home_show/query/', follow=True, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        readjson = eval(response.content)
+        self.assertEqual(readjson['online'], 65535)
+        self.assertEqual(readjson['read'], 4096)
+        self.assertEqual(readjson['sent'], 1024)
+        self.assertEqual(response.status_code, 200)
+
+
